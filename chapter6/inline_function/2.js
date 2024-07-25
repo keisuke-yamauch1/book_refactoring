c9b12894db57e8f6e0ce664f50535808e31c57e6
Keisuke Yamauchi <k.yamauchi@cake.co.jp>
2024-07-25 22:40:01 +0900

    inline function 2 second lines

--- chapter6/inline_function/2.js
+++ chapter6/inline_function/2.js
@@ -1,10 +1,7 @@
 function reportLines(aCustomer) {
     const lines = [];
     lines.push(["name", aCustomer.name]);
+    lines.push(["location", aCustomer.location]);
     gatherCustomerData(lines, aCustomer);
     return lines;
-}
-
-function gatherCustomerData(out, aCustomer) {
-    out.push(["location", aCustomer.location]);
 }
