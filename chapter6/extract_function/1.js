f8c5ec35f50dd0d800cdfe82760fa5b4d7c08558
Keisuke Yamauchi <k.yamauchi@cake.co.jp>
2024-07-25 22:26:45 +0900

    extract function 1 recordDueDate

--- chapter6/extract_function/1.js
+++ chapter6/extract_function/1.js
@@ -8,11 +8,7 @@ function printOwing(invoice) {
         outstanding += o.amount;
     }
 
-    // 締め日の記録
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
-    // 明細の印字
+    recordDueDate(invoice);
     printDetails(invoice, outstanding);
 }
 
@@ -26,4 +22,9 @@ function printDetails(invoice, outstanding) {
     console.log(`name: ${invoice.customer}`);
     console.log(`amount: ${outstanding}`);
     console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+function recordDueDate(invoice) {
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 }
