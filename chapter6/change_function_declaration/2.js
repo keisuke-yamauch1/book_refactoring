71452ccc508e284d17b33c16dd1c58222aeca423
Keisuke Yamauchi <k.yamauchi@cake.co.jp>
2024-08-01 20:41:55 +0900

    change function declaration 2 引数追加

--- chapter6/change_function_declaration/2.js
+++ chapter6/change_function_declaration/2.js
@@ -1,9 +1,9 @@
 class Book {
     addReservation(customer) {
-        this.zz_addReservation(customer);
+        this.zz_addReservation(customer, false);
     }
 
-    zz_addReservation(customer) {
+    zz_addReservation(customer, isPriority) {
         this._reservations.push(customer);
     }
 }
