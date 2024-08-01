54e1bf9c62e547bf2e152fc28ee37bf0e662ec82
Keisuke Yamauchi <k.yamauchi@cake.co.jp>
2024-08-01 20:49:33 +0900

    change function declaration 3 関数の抽出

--- chapter6/change_function_declaration/3.js
+++ chapter6/change_function_declaration/3.js
@@ -1,6 +1,10 @@
 function isNewEngland(aCustomer) {
     const state = aCustomer.address.state;
-    return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(state);
+    return xxNEWinNewEngland(state);
+}
+
+function xxNEWinNewEngland(stateCode) {
+    return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
 }
 
 // caller
