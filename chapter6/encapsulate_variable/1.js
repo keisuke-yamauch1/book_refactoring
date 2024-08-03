538997ed449060d41ae15d781934b25994a4252c
Keisuke Yamauchi <k.yamauchi@cake.co.jp>
2024-08-03 17:32:03 +0900

    encapsulate variable 1 レコードのカプセル化

--- chapter6/encapsulate_variable/1.js
+++ chapter6/encapsulate_variable/1.js
@@ -1,3 +1,13 @@
 let defaultOwnerData = {firstName: "Martin", lastName: "Fowler"};
-export function defaultOwner() { return Object.assign({}, defaultOwnerData); }
+export function defaultOwner() { return new Person(defaultOwnerData); }
 export function setDefaultOwner(arg) { defaultOwnerData = arg; }
+
+class Person {
+    constructor(data) {
+        this._lastName = data.lastName;
+        this._firstName = data.firstName;
+    }
+
+    get lastName() { return this._lastName; }
+    get firstName() { return this._firstName; }
+}
