9cab3923ad3f2426fc083e42c0c593026c1d835c
Keisuke Yamauchi <k.yamauchi@cake.co.jp>
2024-08-04 22:18:27 +0900

    introduce parameter object 1 クラスを定義

--- chapter6/introduce_parameter_object/1.js
+++ chapter6/introduce_parameter_object/1.js
@@ -15,3 +15,11 @@ function readingOutsideRange(station, min, max) {
 
 // caller
 alerts = readingOutsideRange(station, operatingPlan.temperatureFloor, operatingPlan.temperatureCeiling);
+
+class NumberRange {
+    constructor(min, max) {
+        this._data = {min: min, max: max};
+    }
+    get min() { return this._data.min; }
+    get max() { return this._data.max; }
+}
