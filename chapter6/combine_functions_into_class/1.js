eba40eaf325c3193d2c3becf03f034e2cfc25255
Keisuke Yamauchi <k.yamauchi@cake.co.jp>
2024-08-12 12:51:43 +0900

    combine functions into class 1 データクラス作成

--- chapter6/combine_functions_into_class/1.js
+++ chapter6/combine_functions_into_class/1.js
@@ -19,3 +19,16 @@ const basicChargeAmount = calculateBaseCharge(aReading);
 function calculateBaseCharge(aReading) {
     return baseRate(aReading.month, aReading.year) * aReading.quantity;
 }
+
+class Reading {
+    constructor(data) {
+        this._customer = data.customer;
+        this._quantity = data.quantity;
+        this._month = data.month;
+        this._year = data.year;
+    }
+    get customer() {return this._customer;}
+    get quantity() {return this._quantity;}
+    get month() {return this._month;}
+    get year() {return this._year;}
+}
