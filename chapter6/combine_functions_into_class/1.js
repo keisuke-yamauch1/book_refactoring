4289881b6ed17b4defa2703598462ba88edca770
Keisuke Yamauchi <k.yamauchi@cake.co.jp>
2024-08-12 12:57:17 +0900

    combine functions into class 1 client1も置き換える

--- chapter6/combine_functions_into_class/1.js
+++ chapter6/combine_functions_into_class/1.js
@@ -4,8 +4,9 @@ function acquireReading() {
 }
 
 // client 1
-const aReading = acquireReading();
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const baseCharge = aReading.baseCharge;
 
 // client 2
 const aReading = acquireReading();
